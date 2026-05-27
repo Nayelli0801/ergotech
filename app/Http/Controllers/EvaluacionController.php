d31bd06575ab483295cb7b4fc055f4cc411b3d1f
Nayelli0801 <liliamolvera1234@gmail.com>
2026-05-27 12:58:51 -0600

    Update EvaluacionController.php
    quitar relacion ergonomiageneral

--- app/Http/Controllers/EvaluacionController.php
+++ app/Http/Controllers/EvaluacionController.php
@@ -16,19 +16,19 @@ class EvaluacionController extends Controller
     public function index()
     {
         $evaluaciones = Evaluacion::with([
-            'empresa',
-            'sucursal',
-            'puesto',
-            'trabajador',
-            'metodo',
-            'rebaEvaluacion',
-            'rulaEvaluacion',
-            'owasEvaluacion',
-            'nioshEvaluacion',
-            'nom036',
-            'leySilla',
-            'ocraEvaluacion',
-        ])->latest()->get();
+    'empresa',
+    'sucursal',
+    'puesto',
+    'trabajador',
+    'metodo',
+    'rebaEvaluacion',
+    'rulaEvaluacion',
+    'owasEvaluacion',
+    'nioshEvaluacion',
+    'nom036',
+    'leySilla',
+    'ocraEvaluacion',
+])->latest()->get();
 
         return view('evaluaciones.index', compact('evaluaciones'));
     }
@@ -106,9 +106,7 @@ class EvaluacionController extends Controller
             case 'LEY SILLA':
                 return redirect()->route('ley_silla.create', $evaluacion->id);
 
-            case 'ERGONOMIA GENERAL':
-            case 'ERGONOMÍA GENERAL':
-                return redirect()->route('ergonomia_general.create', $evaluacion->id);
+
 
             case 'OCRA':
                 return redirect()->route('ocra.create', $evaluacion->id);
